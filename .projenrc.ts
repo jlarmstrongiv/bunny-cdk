c38686e924dd9a6b17e67a60f007df7e267c7c20
John L. Armstrong IV <20903247+jlarmstrongiv@users.noreply.github.com>
2024-12-11 22:07:03 -0700

    feat: publish to github

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,17 +1,42 @@
-import { awscdk } from 'projen';
+import { awscdk } from "projen";
+
 const project = new awscdk.AwsCdkConstructLibrary({
-  author: 'John L. Armstrong IV',
-  authorAddress: '20903247+jlarmstrongiv@users.noreply.github.com',
-  cdkVersion: '2.1.0',
-  defaultReleaseBranch: 'main',
-  jsiiVersion: '~5.5.0',
-  name: 'bunny-cdk',
+  author: "John L. Armstrong IV",
+  authorAddress: "20903247+jlarmstrongiv@users.noreply.github.com",
+  cdkVersion: "2.1.0",
+  defaultReleaseBranch: "main",
+  jsiiVersion: "~5.5.0",
+  name: "bunny-cdk",
   projenrcTs: true,
-  repositoryUrl: 'https://github.com/20903247+jlarmstrongiv/bunny-cdk.git',
+  repositoryUrl: "https://github.com/20903247+jlarmstrongiv/bunny-cdk.git",
+  gitignore: ["/.env", ".DS_Store", ".AppleDouble", ".LSOverride"],
+  license: "AGPL-3.0-or-later",
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
+
+  publishDryRun: true,
+  packageName: "bunny-cdk",
+
+  publishToNuget: {
+    dotNetNamespace: "BunnyLauncher.BunnyCdk",
+    packageId: "BunnyLauncher.BunnyCdk",
+  },
+  // publishToMaven: {
+  //   javaPackage: 'bunny.cdk',
+  //   mavenArtifactId: 'bunny-cdk',
+  //   mavenGroupId: 'com.bunny-launcher',
+  //   // serverId: 'github',
+  //   // repositoryUrl: 'https://maven.pkg.github.com/bunny-cdk/bunny-cdk',
+  // },
+  publishToPypi: {
+    distName: "bunny-cdk",
+    module: "bunny_cdk",
+  },
+  publishToGo: {
+    moduleName: "github.com/jlarmstrongiv/bunnycdkgo",
+  },
 });
 project.synth();
---
+++ API.md
@@ -0,0 +1,39 @@
+# API Reference <a name="API Reference" id="api-reference"></a>
+
+
+
+## Classes <a name="Classes" id="Classes"></a>
+
+### Hello <a name="Hello" id="bunny-cdk.Hello"></a>
+
+#### Initializers <a name="Initializers" id="bunny-cdk.Hello.Initializer"></a>
+
+```typescript
+import { Hello } from 'bunny-cdk'
+
+new Hello()
+```
+
+| **Name** | **Type** | **Description** |
+| --- | --- | --- |
+
+---
+
+#### Methods <a name="Methods" id="Methods"></a>
+
+| **Name** | **Description** |
+| --- | --- |
+| <code><a href="#bunny-cdk.Hello.sayHello">sayHello</a></code> | *No description.* |
+
+---
+
+##### `sayHello` <a name="sayHello" id="bunny-cdk.Hello.sayHello"></a>
+
+```typescript
+public sayHello(): string
+```
+
+
+
+
+
